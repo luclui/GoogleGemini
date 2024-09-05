98183969b5cd28d7fdb215973565eab47ca9ef44
Lucas Liggi <lucasvechiliggi@gmail.com>
2024-09-05 16:16:06 -0300

    Adiciona o JS a página index

---
+++ dados.js
@@ -0,0 +1,48 @@
+let dados = [
+    {
+        titulo: "Lucão",
+        descricao: "O gigante do vôlei brasileiro! Conhecido pelos seus bloqueios poderosos, o Lucão é um dos maiores centrais do mundo. Já conquistou vários títulos importantes e é ídolo da nação!",
+        link: "https://pt.wikipedia.org/wiki/Lucas_Saatkamp"
+    },
+    {
+        titulo: "Bruninho",
+        descricao: "O maestro da seleção! O Bruninho é o cérebro das nossas jogadas. Com seus passes incríveis, ele comanda o time como ninguém. Um verdadeiro gênio do vôlei!",
+        link: "https://pt.wikipedia.org/wiki/Bruno_Rezende"
+    },
+    {
+        titulo: "Natália Guitler",
+        descricao: "A força da seleção feminina! A Natália é uma atacante de respeito, com uma potência incrível nos seus ataques. Já conquistou vários títulos e é um dos maiores orgulhos do vôlei brasileiro.",
+        link: "https://pt.wikipedia.org/wiki/Natália_Guitler"
+    },
+    {
+        titulo: "Robertlandy Simón",
+        descricao: "O muro cubano! O Robertlandy é conhecido pelos seus bloqueios poderosos. Um gigante nas quadras que já jogou por vários times europeus. É um dos melhores centrais do mundo!",
+        link: "https://pt.wikipedia.org/wiki/Robertlandy_Simón"
+    },
+    {
+        titulo: "Paola Egonu",
+        descricao: "A promessa italiana! A Paola é considerada uma das maiores promessas do vôlei feminino. Com seu talento e versatilidade, ela já conquistou o coração dos fãs. Prepare-se para ver essa estrela brilhar ainda mais!",
+        link: "https://pt.wikipedia.org/wiki/Paola_Egonu"
+    },
+    {
+        titulo: "Wallace de Souza",
+        descricao: "O destruidor! O Wallace é conhecido pelos seus saques poderosos que deixam qualquer um assustado. Um dos maiores pontuadores do vôlei, ele sempre leva a torcida à loucura.",
+        link: "https://pt.wikipedia.org/wiki/Wallace_de_Souza"
+    },
+    {
+        titulo: "Tandara Caixeta",
+        descricao: "A camaleoa do vôlei! A Tandara é super versátil e consegue jogar em várias posições. Uma jogadora completa que sempre entrega tudo de si em quadra.",
+        link: "https://pt.wikipedia.org/wiki/Tandara_Caixeta"
+    },
+    {
+        titulo: "Yuki Ishikawa",
+        titulo: "Yuki Ishikawa",
+        descricao: "O famoso japonês! Yuki é conhecido por seus ataques poderosos e versatilidade. Ele é uma estrela em ascensão no vôlei asiático.",
+        link: "https://en.wikipedia.org/wiki/wiki/Yuki_Ishikawa"
+    },
+    {
+        titulo: "Tijana Boskovic",
+        descricao: "A máquina sérvia! A Tijana é uma das melhores atacantes do mundo. Sua precisão e força são impressionantes. Prepare-se para ver ataques incríveis!",
+        link: "https://en.wikipedia.org/wiki/Tijana_Boskovic"
+    },
+];
